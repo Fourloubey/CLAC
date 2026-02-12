f2875868405785337b771c34ca9e535f3514a0e2
PacoAugustin <gaetan.fourloubey@gmail.com>
2026-02-12 23:06:18 +0100

    Update App.tsx basename="/clac"

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/clac">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/agence" element={<Agence />} />
